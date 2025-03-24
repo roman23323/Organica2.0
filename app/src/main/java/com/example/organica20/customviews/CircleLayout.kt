2fb94ffe6088257a31f2b5260afbce5d23884082
Roman <ignatenko_roman.s@mail.ru>
2025-03-24 23:15:44 +0300

    feat: added customizable elements placement, radius adjustment and line drawing
    - Added 'app:angle' to customize the distribution of elements around the circle.
    - Added 'app:radiusMultiplier' to adjust the radius of the circle.
    - Added lines connecting the center element with outer elements.

--- app/src/main/java/com/example/organica20/customviews/CircleLayout.kt
+++ app/src/main/java/com/example/organica20/customviews/CircleLayout.kt
@@ -1,10 +1,17 @@
 package com.example.organica20.customviews
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
+import android.util.Log
 import android.view.ViewGroup
+import com.example.organica20.R
 import kotlin.math.cos
+import kotlin.math.max
 import kotlin.math.min
+import kotlin.math.pow
 import kotlin.math.sin
 
 class CircleLayout @JvmOverloads constructor(
@@ -12,36 +19,87 @@ class CircleLayout @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : ViewGroup(context, attrs, defStyleAttr){
+
+    private var angle: Float = 0f
+    private var radiusMultiplier: Int = 4
+    private val paint = Paint().apply {
+        color = Color.BLACK
+        strokeWidth = 2f
+        style = Paint.Style.STROKE
+    }
+
+    init {
+        setBackgroundColor(Color.TRANSPARENT)
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.CircleLayout)
+        angle = typedArray.getFloat(
+            R.styleable.CircleLayout_angle,
+            0f
+        )
+        radiusMultiplier = typedArray.getInteger(
+            R.styleable.CircleLayout_radiusMultiplier,
+            4
+        )
+        typedArray.recycle()
+    }
+
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        var size = min(MeasureSpec.getSize(widthMeasureSpec), MeasureSpec.getSize(heightMeasureSpec))
+        if (angle == 0f && childCount > 0) {
+            angle = ((2 * Math.PI) / childCount).toFloat()
+        }
+
+        var desiredSize = 0
 
         for (i in 0 until childCount) {
             val child = getChildAt(i)
             measureChild(child, widthMeasureSpec, heightMeasureSpec)
-            size = size.coerceAtMost(3 * child.measuredWidth)
+            desiredSize = max(desiredSize, max(child.measuredWidth, child.measuredHeight))
         }
+
+        desiredSize *= radiusMultiplier
+
+        val measuredWidth = when (MeasureSpec.getMode(widthMeasureSpec)) {
+            MeasureSpec.EXACTLY -> MeasureSpec.getSize((widthMeasureSpec))
+            MeasureSpec.AT_MOST -> min(MeasureSpec.getSize((widthMeasureSpec)), desiredSize)
+            else -> desiredSize
+        }
+
+        val measuredHeight = when (MeasureSpec.getMode(heightMeasureSpec)) {
+            MeasureSpec.EXACTLY -> MeasureSpec.getSize((heightMeasureSpec))
+            MeasureSpec.AT_MOST -> min(MeasureSpec.getSize((heightMeasureSpec)), desiredSize)
+            else -> desiredSize
+        }
+
+        val size = max(measuredWidth, measuredHeight)
+
         setMeasuredDimension(size, size)
     }
 
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
-        val centerX = (r - l) / 2
-        val centerY = (b - t) / 2
-        val radius = centerX.coerceAtMost(centerY).toFloat()
-
         val childCount = childCount
         if (childCount == 0) return
 
-        val angleStep = (2 * Math.PI / childCount).toFloat()
+        val centerX = (r - l) / 2
+        val centerY = (b - t) / 2
+        val radius = centerX.coerceAtMost(centerY).toFloat()
 
         for (i in 0 until childCount) {
             val child = getChildAt(i)
             val childWidth = child.measuredWidth
             val childHeight = child.measuredHeight
 
-            val angle = i * angleStep
+            val angle = (i - 1) * angle
 
-            val childCenterX = centerX + (cos(angle) * (radius - childWidth / 2)).toInt()
-            val childCenterY = centerY + (sin(angle) * (radius - childHeight / 2)).toInt()
+            val childCenterX = if (i != 0) {
+                centerX + (cos(angle) * (radius - childWidth / 2)).toInt()
+            } else {
+                centerX
+            }
+            val childCenterY = if (i != 0) {
+                centerY + (sin(angle) * (radius - childHeight / 2)).toInt()
+            } else {
+                centerY
+            }
+            Log.d("ON_LAYOUT", "Индекс: $i, (x, y): ($childCenterX, $childCenterY), угол: $angle")
 
             child.layout(
                 childCenterX - childWidth / 2,
@@ -49,6 +107,38 @@ class CircleLayout @JvmOverloads constructor(
                 childCenterX + childWidth / 2,
                 childCenterY + childHeight / 2
             )
+        }
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        if (childCount == 0) return
+
+        val mainChild = getChildAt(0)
+        val mWidth = mainChild.width
+        val mHeight = mainChild.height
+        val mOffsetRadius = (mWidth.toDouble().div(2).pow(2) + mHeight.toDouble().div(2).pow(2)).pow(0.5).toFloat()
+
+        val mCenterX = mainChild.left + mainChild.width / 2
+        val mCenterY = mainChild.top + mainChild.height / 2
+
+        for (i in 1 until childCount) {
+            val child = getChildAt(i)
+            val cWidth = child.width
+            val cHeight = child.height
+            val cCenterX = child.left + child.width / 2
+            val cCenterY = child.top + child.height / 2
+            val cOffsetRadius = (cWidth.toDouble().div(2).pow(2) + cHeight.toDouble().div(2).pow(2)).pow(0.5).toFloat()
+
+            val angle = (i - 1) * angle
+
+            val startX = mCenterX + cos(angle) * mOffsetRadius
+            val startY = mCenterY + sin(angle) * mOffsetRadius
+            val endX = (cCenterX + cos(2 * Math.PI - (Math.PI - angle)) * cOffsetRadius).toFloat()
+            val endY = (cCenterY + sin(2 * Math.PI - (Math.PI - angle)) * cOffsetRadius).toFloat()
+
+            canvas.drawLine(startX, startY, endX, endY, paint)
         }
     }
 }
