193c46af9163b70010aee8b3992e7ef611eb7e1c
Roman <ignatenko_roman.s@mail.ru>
2025-04-24 16:15:30 +0300

    feat: add fragments for main menu tabs

---
+++ app/src/main/java/com/example/organica20/ui/fragment/SettingsFragment.kt
@@ -0,0 +1,17 @@
+package com.example.organica20.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.organica20.R
+
+class SettingsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+}
